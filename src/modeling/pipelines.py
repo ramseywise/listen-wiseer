2689f0c75ebea4a2bb7d1d2d1e64eb3d2bd27be3
ramseywise <ramsey.wise@gmail.com>
2023-12-28 15:25:03 +0100

    update pipeline with feature selection

--- src/modeling/pipelines.py
+++ src/modeling/pipelines.py
@@ -1,14 +1,14 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
-#from sklearn.model_selection import KFold
+from sklearn.feature_selection import RFE
+
+# from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.pipeline import Pipeline
-from modeling.const import *
-from modeling.preprocessing import *
-from sklearn.ensemble import IsolationForest
+
 from sklearn.metrics import (
     confusion_matrix,
     f1_score,
@@ -17,10 +17,20 @@ from sklearn.metrics import (
     roc_auc_score,
     accuracy_score,
 )
+from modeling.const import *
+from modeling.preprocessing import *
+import logger
+
+os.chdir("/Users/wiseer/Documents/github/listen-wiseer/src/")
+log = logger.get_logger("app")
+
+
+# TODO: Turn into class and separate functions
 
 
 def fit_and_evaluate(y_true, y_pred):
     """Return metrics to evaluate model performance."""
+    log.info("Evaluating models")
     f1 = round(f1_score(y_true, y_pred), 2)
     roc_auc = round(roc_auc_score(y_true, y_pred), 2)
     accuracy = round(accuracy_score(y_true, y_pred), 2)
@@ -31,35 +41,112 @@ def fit_and_evaluate(y_true, y_pred):
     return accuracy, precision, recall, f1, roc_auc  # specificicty
 
 
-def return_model_metrics(df):
-    # NOTE: these models only use fit, not fit_transform
-    pipelines = []
-    pipelines.append(Pipeline(steps=[("lr", LogisticRegression())]))
-    pipelines.append(Pipeline(steps=[("rf", RandomForestClassifier())]))
-    pipelines.append(Pipeline(steps=[("dt", DecisionTreeClassifier())]))
-    pipelines.append(Pipeline(steps=[("if", IsolationForest())]))
-    pipelines.append(Pipeline(steps=[("kn", KNeighborsClassifier())]))
+def return_model_metrics(scores):
+    results = pd.DataFrame(scores)
+    results["model"] = [
+        "Logistic Regression",
+        "Decision Tree",
+        "Random Forest",
+    ]
+    results = results[["model", "accuracy", "precision", "recall", "f1", "roc_auc"]]
+    return results
 
+
+def return_feature_selection(selected_features):
+    log.info("Selecting features")
+    feature_selection = pd.DataFrame(selected_features).T
+    feature_selection.columns = ["Logistic", "Decision Tree", "Random Forest"]
+
+    return feature_selection
+
+
+def config_model_pipeline():
+    """Builds pipeline for sklearn models and returns model metrics."""
+    pipelines = []
+    pipelines.append(
+        Pipeline(
+            [
+                (
+                    "scaler",
+                    MinMaxScaler(),
+                ),
+                (
+                    "rfe",
+                    RFE(
+                        estimator=LogisticRegression(),
+                        n_features_to_select=15,
+                    ),
+                ),
+                ("classifier", LogisticRegression()),
+            ]
+        )
+    )
+    pipelines.append(
+        Pipeline(
+            [
+                (
+                    "scaler",
+                    MinMaxScaler(),
+                ),
+                (
+                    "rfe",
+                    RFE(
+                        estimator=DecisionTreeClassifier(),
+                        n_features_to_select=15,
+                    ),
+                ),
+                ("classifier", DecisionTreeClassifier()),
+            ]
+        )
+    )
+    pipelines.append(
+        Pipeline(
+            [
+                (
+                    "scaler",
+                    MinMaxScaler(),
+                ),
+                (
+                    "rfe",
+                    RFE(
+                        estimator=RandomForestClassifier(n_estimators=100),
+                        n_features_to_select=15,
+                    ),
+                ),
+                ("classifier", RandomForestClassifier(n_estimators=100)),
+            ]
+        )
+    )
+    return pipelines
+
+
+def fit_models(df):
     # TODO: incorporate train data into data flow pipeline when DB is setup
-    X = transform_feature_data(df)
+    X = transform_feature_data(df)  # TODO: problem with transofming in pipeline
     y = pd.read_csv(
         "/Users/wiseer/Documents/github/listen-wiseer/src/data/faves.csv",
         index_col=0,
     )
-    y.set_index("id", inplace=True)
+    y = np.array(y["faves"]).ravel()
 
     # TODO: add smote/k-fold validation
+
     # Splitting Dataset
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=0
     )
 
+    log.info("Fitting models")
+    pipelines = config_model_pipeline()
     scores = []
+    selected_features = []
     for model in pipelines:
         model.fit(X_train, y_train)
+        feature_importance = model.named_steps["rfe"].support_
+        rfe_features = list(X.columns[feature_importance])
+        selected_features.append(rfe_features)
         y_true = y_test
         y_pred = model.predict(X_test)
-        # y_pred = np.where(y_pred == 1, 0, 1)
         accuracy, precision, recall, f1, roc_auc = fit_and_evaluate(y_true, y_pred)
         scores.append(
             {
@@ -70,12 +157,7 @@ def return_model_metrics(df):
                 "roc_auc": roc_auc,
             }
         )
-    results = pd.DataFrame(scores)
-    results["model"] = [
-        "Logistic Regression",
-        "Random Forest",
-        "Decision Tree",
-        "Isolation Forest",
-    ]
-    results = results[["model", "accuracy", "precision", "recall", "f1", "roc_auc"]]
-    return results
+    metrics = return_model_metrics(scores)
+    feature_selection = return_feature_selection(selected_features)
+    # TODO: add model selection and hyperparameter tuning; when model is selected retraino on selected features
+    return metrics, feature_selection
